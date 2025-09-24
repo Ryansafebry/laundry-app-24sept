32db55b10ecedf49c89cc69b1f28697e14176e61
[dyad] <arnada@betterworks.id>
2025-09-24 14:29:43 +0700

    [dyad] Membuat dashboard pemesanan laundry - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import LaundryDashboard from "./pages/LaundryDashboard"; // Import the new dashboard
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/dashboard" element={<LaundryDashboard />} /> {/* New route for dashboard */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
