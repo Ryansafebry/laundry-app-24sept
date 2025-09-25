d91dcb366aa0985379d0c1c668f3b4707bce3614
[dyad] <arnada@betterworks.id>
2025-09-25 12:53:52 +0700

    [dyad] Menambahkan halaman riwayat pesanan - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+"use client";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LaundryDashboard from "./pages/LaundryDashboard";
-import History from "./pages/History";
-import AdminDashboard from "./pages/AdminDashboard"; // Import the new AdminDashboard page
+import AdminDashboard from "./pages/AdminDashboard";
+import OrderHistory from "./pages/OrderHistory"; // Import komponen OrderHistory
+import "./App.css";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<LaundryDashboard />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/admin" element={<AdminDashboard />} /> {/* New route for Admin Dashboard */}
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LaundryDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/history" element={<OrderHistory />} /> {/* Tambahkan rute baru */}
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
